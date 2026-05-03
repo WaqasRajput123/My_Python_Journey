53de1975188691bfeaf278fd863811d35d5982b4
Waqas Rajput <waqascreator788@gmail.com>
2026-05-03 18:07:12 +0500

    first update

---
+++ Sum_Calculation.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+def Sum_Calculate(num1, num2):
+    sum = num1 + num2
+    return sum
+
+print(Sum_Calculate(num1, num2))
