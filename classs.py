88eb6d57388fbf2c481050a853cf3314bba4a850
Waqas Rajput <waqascreator788@gmail.com>
2026-05-06 00:18:45 +0500

    update

---
+++ classs.py
@@ -0,0 +1,19 @@
+class Student:
+    name = "waqas Hanif"
+    rollnumber = "23011598-154"
+
+    # def __init__(self):
+    #     print("Your name is", self.name, "and rollnumber is", self.rollnumber)
+
+    def Student_Marks_Average(self, marks):
+        avg = 0
+        sum = 0
+        for i in marks:
+            sum = sum + i
+        
+        avg = sum / i
+        print(avg)
+
+
+s1 = Student()
+s1.Student_Marks_Average([1, 2, 3, 4])
